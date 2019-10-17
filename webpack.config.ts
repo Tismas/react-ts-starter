7dba26b4ab548aaeb1c00efb4dc5a84ea163b12e
Adam Golan <golana01@stepstone.com>
2019-10-17 16:34:17 +0200

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+import * as path from 'path'
+import { Configuration } from 'webpack'
+import HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config: Configuration = {
+  mode: 'development',
+  entry: ['babel-polyfill', './src/index.tsx'],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader'
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config
