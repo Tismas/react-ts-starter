1bdd56185a9b18029c39198399f77686a74652d0
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2022-05-26 15:34:05 +0200

    Production ready

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,15 +2,17 @@ import path from 'path';
 import Webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+const isProduction = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
 const config: Webpack.Configuration = {
   entry: ['babel-polyfill', './src/index.tsx'],
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: isProduction,
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js',
+    filename: isProduction ? 'app.[contenthash].js' : 'app.js',
     publicPath: '/',
   },
   plugins: [
